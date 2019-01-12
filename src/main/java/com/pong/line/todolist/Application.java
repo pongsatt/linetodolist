74e7cf41707b885a53a3ec48f661c6476a04c57e
ponggit <pongsatt@gmail.com>
2019-01-12 23:13:39 +0700

    Clean up

--- src/main/java/com/pong/line/todolist/Application.java
+++ src/main/java/com/pong/line/todolist/Application.java
@@ -2,17 +2,14 @@ package com.pong.line.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Application {
-
-    @RequestMapping("/")
-    String home() {
-        return "This is a line todo list chat bot.";
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
 }
